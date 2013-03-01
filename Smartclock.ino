e42b34248c52c46939dfdca0c995caf4d69203f9
Rob Connolly <rob@webworxshop.com>
2013-03-02 12:38:08 +1300

    Add skeleton code for comms.
    Add skeleton code for comms, MQTT over bluetooth RFCOMM. Add
    support for light sensor and disabling of display on low light
    levels.
    
    NEXT: Need to test this and then get comms stuff working.

--- Smartclock.ino
+++ Smartclock.ino
@@ -2,16 +2,47 @@
 #include "Time.h"
 #include "TimeAlarms.h"
 #include "Serial7SegmentDisplay.h"
+#include "SerialClient.h"
+#include "PubSubClient.h"
+#include "Bluetooth.h"
  
 // Temperature Sensor on analog input 0
-#define TEMP_SENSOR	0
+#define TEMP_SENSOR	                0
 // Light Sensor on analog input 1
-#define LIGHT_SENSOR	1
+#define LIGHT_SENSOR	            1
+// Battery Sensor on analog input 2
+#define BATTERY_SENSOR              2
 
-Serial7SegmentDisplay Display;
+// Number of sensor readings to average
+#define SENSOR_AVERAGE_READINGS     200
 
+
+
+// local variables for sensors and program state
 bool displayTime = true;
 unsigned int temperature = 0, temp_acc = 0, temp_cnt = 0;
+unsigned long battery = 0, batt_acc = 0, batt_cnt = 0;
+unsigned int lightlevel = 0, light_acc = 0, light_cnt = 0;
+
+// configuration stuff pushed from server
+unsigned int light_threshhold = 1000;
+
+// static configuration stuff
+byte server[] = { 10, 0, 0, 194 }; // not actually needed for serial PTP link
+
+// function defs
+void pubSubCallback(char* topic, byte* payload, unsigned int length);
+
+// hardware driver interface defs
+//SerialClient serClient;
+//PubSubClient client(server, 1883, callback, serClient);
+Serial7SegmentDisplay Display;
+Bluetooth bluetooth;
+
+void pubSubCallback(char* topic, byte* payload, unsigned int length)
+{
+  // handle message arrived
+}
 
 String formatDigits(int d)
 {
@@ -22,17 +53,19 @@ String formatDigits(int d)
 
 void displayUpdate() 
 {
-    Serial.println("display callback");
     Display.clear();
-    if(displayTime)
+    if(lightlevel >= light_threshold)
     {
-        Display.writeSpecial(Display.COLON);   
-        Display.write(formatDigits(hour()) + formatDigits(minute()));
-    }
-    else
-    {
-        Display.writeSpecial(Display.DECIMAL1 | Display.APOSTROPHE);
-        Display.write(String(temperature) + " ");
+        if(displayTime)
+        {
+            Display.writeSpecial(Display.COLON);   
+            Display.write(formatDigits(hour()) + formatDigits(minute()));
+        }
+        else
+        {
+            Display.writeSpecial(Display.DECIMAL1 | Display.APOSTROPHE);
+            Display.write(String(temperature) + " ");
+        }
     }
     displayTime = !displayTime;
 }
@@ -42,32 +75,60 @@ void sensorsUpdate()
     // read sensors
     float a0 = analogRead(TEMP_SENSOR);
     float a1 = analogRead(LIGHT_SENSOR);
-    
+    float a2 = analogRead(BATTERY_SENSOR);
+
     temp_acc += (((a0*3.3)/1023.0) - 0.5)*1000;
     temp_cnt++;
-    if(temp_cnt >= 199)
+    if(temp_cnt >= SENSOR_AVERAGE_READINGS)
     {
         temperature = temp_acc/temp_cnt;
-        Serial.println(temp_acc);
-        Serial.println(temp_cnt);
         temp_acc = 0;
         temp_cnt = 0;
+    }
+
+    light_acc += ((a1*3.3)/1023.0);
+    light_cnt++;
+    if(light_cnt >= SENSOR_AVERAGE_READINGS)
+    {
+        lightlevel = light_acc/light_cnt;
+        light_acc = 0;
+        light_cnt = 0;
+    }
+
+    batt_acc += ((a2*3.3)/1023.0)*2820;
+    batt_cnt++;
+    if(batt_cnt >= SENSOR_AVERAGE_READINGS)
+    {
+        battery = batt_acc/batt_cnt;
+        batt_acc = 0;
+        batt_cnt = 0;
     }
 }
 
 void printSensors()
 {
-    Serial.println(temperature);
+    //Serial.println(temperature);
 }
 
 void setup()
 {
     Display.begin(10);
-    setTime(17, 43, 0, 9, 1, 2013);
+    setTime(17, 20, 0, 9, 1, 2013);
+
+    Serial.begin(57600);
+    //while(!bluetooth.beginCMD())
+    //{
+    //    delay(100);
+    //}
 
     // Analog setup
     analogReference(DEFAULT);
-    Serial.begin(9600);
+    
+    //if(client.connect("smartclock"))
+    //{
+    //    client.publish("/devices/" + deviceName + "/status", "online");
+    //    // subscribe to topics
+    //}
 
     // Alarm setup
     displayUpdate();
