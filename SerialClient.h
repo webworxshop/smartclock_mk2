e09c7109dfc69d8548eb59dbb195123d67953581
Rob Connolly <rob@webworxshop.com>
2013-07-01 20:47:32 +1200

    Add copyright notices. Update bluetooth submodule.

--- SerialClient.h
+++ SerialClient.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *  Copyright 2013 Rob Connolly <rob@webworxshop.com                       *
+ *                                                                         *
+ *  This file is part of Smartclock MK2.                                   *
+ *                                                                         *
+ *  Smartclock MK2 is free software: you can redistribute it and/or modify *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  Smartclock MK2 is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with Smartclock MK2.  If not, see <http://www.gnu.org/licenses/>.*
+ ***************************************************************************/
+
 #ifndef __SERIAL_CLIENT_H
 #define __SERIAL_CLIENT_H
 	
